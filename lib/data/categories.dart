17408d8f031c7ca5c355b011bb1193ccc0a41440
Harsh Rathod <haarsh.rathod.7104@gmail.com>
2025-02-25 15:38:41 +0530

    Prefill categories

---
+++ lib/data/categories.dart
@@ -0,0 +1,19 @@
+// Prefill all  categories
+
+import 'package:flutter/material.dart';
+
+List<Map<String, dynamic>> categories = [
+  {"name": "Housing", "icon": Icons.house.codePoint},
+  {"name": "Transportation", "icon": Icons.emoji_transportation.codePoint},
+  {"name": "Food", "icon": Icons.restaurant.codePoint},
+  {"name": "Utilities", "icon": Icons.category.codePoint},
+  {"name": "Insurance", "icon": Icons.health_and_safety.codePoint},
+  {"name": "Medical & Healthcare", "icon": Icons.medical_information.codePoint},
+  {
+    "name": "Saving, Investing, & Debt Payments",
+    "icon": Icons.attach_money.codePoint
+  },
+  {"name": "Personal Spending", "icon": Icons.house.codePoint},
+  {"name": "Recreation & Entertainment", "icon": Icons.tv.codePoint},
+  {"name": "Miscellaneous", "icon": Icons.library_books_sharp.codePoint},
+];
